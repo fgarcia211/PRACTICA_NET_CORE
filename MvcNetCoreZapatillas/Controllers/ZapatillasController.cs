c61229f67a5881e0cc4c60f68602e7b3dd08c498
fgarcia211 <fergarcia002100@outlook.es>
2023-03-16 13:41:36 +0100

    Add project files.

---
+++ MvcNetCoreZapatillas/Controllers/ZapatillasController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MvcNetCoreZapatillas.Models;
+using MvcNetCoreZapatillas.Repositories;
+
+namespace MvcNetCoreZapatillas.Controllers
+{
+    public class ZapatillasController : Controller
+    {
+        private RepositoryZapatillas repo;
+
+        public ZapatillasController(RepositoryZapatillas repo)
+        {
+            this.repo = repo;
+        }
+
+        public IActionResult VistaZapatillas()
+        {
+            return View(this.repo.GetAllZapatillas());
+        }
+
+        public IActionResult DetallesZapatilla(int idproducto)
+        {
+            return View(this.repo.GetOneZapatilla(idproducto));
+        }
+
+        public async Task<IActionResult> _PaginadoPartial(int idproducto, int? posicion)
+        {
+            if (posicion == null || posicion < 1)
+            {
+                posicion = 1;
+            }
+
+            int numeroimagenes = this.repo.CountImagenesZapatilla(idproducto);
+
+            if (posicion > numeroimagenes)
+            {
+                posicion = numeroimagenes;
+            }
+
+            ImagenZapatilla imagen = this.repo.GetImagenZapatilla(idproducto, posicion.Value);
+
+            ViewData["REGISTROS"] = numeroimagenes;
+            ViewData["POSICION"] = posicion;
+
+            return PartialView("_PaginadoPartial", imagen);
+        }
+    }
+}
---
+++ MvcNetCoreZapatillas/Repositories/RepositoryZapatillas.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+using MvcNetCoreZapatillas.Data;
+using MvcNetCoreZapatillas.Models;
+using System.Data;
+
+#region SQL SERVER
+//VUESTRO PROCEDIMIENTO DE PAGINACION DE IMAGENES DE ZAPATILLAS
+/*CREATE OR ALTER PROCEDURE SP_GETZAPATILLAID
+(@IDPRODUCTO INT)
+AS
+	SELECT * FROM ZAPASPRACTICA WHERE IDPRODUCTO = @IDPRODUCTO
+GO
+
+CREATE OR ALTER PROCEDURE SP_IMAGENES_PRODUCTO
+(@POSICION INT, @IDPRODUCTO INT)
+AS
+   SELECT * FROM
+        (SELECT CAST(
+            ROW_NUMBER() OVER (ORDER BY IDIMAGEN) AS INT) AS POSICION, 
+            IDIMAGEN, IDPRODUCTO, IMAGEN
+        FROM IMAGENESZAPASPRACTICA
+        WHERE IDPRODUCTO = @IDPRODUCTO) AS QUERY
+    WHERE QUERY.POSICION = @POSICION
+GO*/
+#endregion
+
+namespace MvcNetCoreZapatillas.Repositories
+{
+    public class RepositoryZapatillas
+    {
+        private ZapatillasContext context;
+
+        public RepositoryZapatillas(ZapatillasContext context)
+        {
+            this.context = context;
+        }
+
+        public List<Zapatilla> GetAllZapatillas()
+        {
+            return this.context.Zapatillas.ToList();
+        }
+
+        public Zapatilla GetOneZapatilla(int idproducto)
+        {
+            string sql = "SP_GETZAPATILLAID @IDPRODUCTO";
+            SqlParameter pamId = new SqlParameter("@IDPRODUCTO", idproducto);
+            var consulta = this.context.Zapatillas.FromSqlRaw(sql,pamId);
+            Zapatilla zapa= consulta.AsEnumerable().FirstOrDefault();
+            return zapa;
+        }
+
+        public ImagenZapatilla GetImagenZapatilla(int idproducto, int posicion)
+        {
+            string sql = "SP_IMAGENES_PRODUCTO @POSICION, @IDPRODUCTO";
+
+            SqlParameter pamPos = new SqlParameter("@POSICION", posicion);
+            SqlParameter pamIdPro = new SqlParameter("@IDPRODUCTO", idproducto);
+
+            var consulta = this.context.ImagenesZapatillas.FromSqlRaw(sql, pamPos, pamIdPro);
+            ImagenZapatilla imagenZapatillas = consulta.AsEnumerable().FirstOrDefault();
+
+            return imagenZapatillas;
+
+        }
+
+        public int CountImagenesZapatilla(int idproducto)
+        {
+            return this.context.ImagenesZapatillas.Where(z => z.IdProducto == idproducto).Count();
+        }
+
+    }
+}
